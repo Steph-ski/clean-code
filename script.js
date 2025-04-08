8e1a46748785cbbbb831cb424b7a9f9a1faec97d
Steph Johns <steph.johns@mrgsystems.co.uk>
2025-04-08 19:10:43 +0100

    code refactor to only use  single function

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function f1() {
+function f1(targetDate, elementToUpdate) {
     var p = new Date();
-    var q = new Date("2025-12-25");
+   // var q = new Date("2025-12-25");
   
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element");
+    var variable = Math.round((targetDate - p) / (1000 * 60 * 60 * 24));
+    var variable2 = document.getElementById(elementToUpdate);
     variable2.innerText = variable;
   }
   
-  function f2() {
+ /* function f2() {
     var p = new Date();
     var q = new Date("2026-01-01");
   
@@ -16,8 +15,15 @@ function f1() {
   
     var variable2 = document.getElementById("my-element2");
     variable2.innerText = variable;
-  }
+  }*/
   
-  f1();
-  f2();
+    var targetDate1 = new Date("2025-12-25");
+    var elementToUpdate1 = "my-element";
+
+    var targetDate2 = new Date("2026-01-01");
+    var elementToUpdate2 = "my-element2"; 
+  
+  f1(targetDate1, elementToUpdate1);
+  f1(targetDate2, elementToUpdate2);
+  //f2();
   
