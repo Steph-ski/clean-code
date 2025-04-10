dd198608d86dab2606a033897a3facb4aec2cd0a
Steph Johns <steph.johns@mrgsystems.co.uk>
2025-04-10 06:45:03 +0100

    commented out code removed

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/*function f1(targetDate, elementToUpdate) {
-    var p = new Date();
-    var dateDiffInDays = Math.round((targetDate - p) / (1000 * 60 * 60 * 24));
-    var countDownValue = document.getElementById(elementToUpdate);
-    countDownValue.innerText = dateDiffInDays;
-  }*/
-
 function calculateAndSetCountDownValue(targetDate, elementToUpdate) {
     var dateDiffInDays = calculateDateDiffInDays(targetDate);
     var countDownValue = document.getElementById(elementToUpdate);   
@@ -23,9 +16,6 @@ var christmasCountDownValue = "christmas-countdown-value";
 var newYear = new Date("2026-01-01");
 var newYearCountDownValue = "new-year-countdown-value"; 
   
-//f1(christmas, christmasCountDownValue);
-//f1(newYear, newYearCountDownValue);
-
 calculateAndSetCountDownValue(christmas, christmasCountDownValue);
 calculateAndSetCountDownValue(newYear, newYearCountDownValue);
 
